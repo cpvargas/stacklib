3ac4dd83f782f4be5dac5e30de372654196adf8e
cpvargas <cpvarga1@uc.cl>
2016-11-06 22:22:10 -0300

    Add script ez_stack.py, stack beams on a blank map

---
+++ ez_stack.py
@@ -0,0 +1,52 @@
+'''
+Simple script to test some functions and methods
+
+Creates a fake catalog of 100 sources at [RA,DEC] inside fullmap area
+at each position pastes beammaps of amplitude -150 on a zero fullmap, 
+then it performs a stack of all beams.
+'''
+
+from datetime import datetime
+startTime = datetime.now()
+
+import stacklib as sl
+import numpy as np
+
+import os
+path = os.environ["HOME"] + '/FILES/' 
+
+m = path + 'ACT_148_equ_season_3_1way_v3_src_free.fits'
+w = path + 'ACT_148_equ_season_3_1way_calgc_strictcuts2_weights.fits'
+b = path + 'profile_AR1_2009_pixwin_130224.txt'
+s = path + 'Equa_mask_15mJy.fits'
+
+RA0 = 55.
+RA1 = 324.
+DEC0 = -1.5
+DEC1 = 1.5
+
+M = sl.StackMap(m,w,b,s,RA0,RA1,DEC0,DEC1)
+
+M.fullmap = np.abs(M.fullmap*0)
+
+cat = sl.fakecatalog(100)
+
+psize = np.abs(M.maphdr['CDELT1'])
+
+Beam = -150*sl.beam(b,psize,10)
+              
+for item in cat:
+    loc = M.getpix(item[0],item[1])
+    M.fullmap = sl.pastemap(M.fullmap, Beam, loc)
+    
+M.setsubmapL(16)
+
+M.setstackmap()
+
+for item in cat:
+    M.setsubmap(item[0],item[1])    
+    M.stacksubmap()
+    
+M.finishstack()
+
+print datetime.now() - startTime
